90dbfde4fd61e18da641e7e96f82a882c6a6770c
esG3 <esmeG3@users.noreply.github.com>
2024-01-17 19:59:16 +0000

    created function to display current weather (using api)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
+function refreshWeather(responce) {
+let temperatureElement = document.querySelector("#temperature-today")
+let temperature = responce.data.temperature.current;
+let cityWeatherElement = document.querySelector("#city-weather");
+
+cityWeatherElement.innerHTML = responce.data.city;
+temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+let apiKey = "982865b6c020427a6364ef4b31o6dt0f";
+let apiUrl =
+  `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(refreshWeather);
+}
+
+
 function displayCityName(event) {
 event.preventDefault();
 let searchInput = document.querySelector("#form-search-input");
-let cityWeatherElement = document.querySelector("#city-weather");
-cityWeatherElement.innerHTML = searchInput.value;
+
+searchCity(searchInput.value)
 }
 
 let searchFormElement = document.querySelector("#search-form");
