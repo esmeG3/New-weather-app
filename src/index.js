f9fb9e5714db997804e0f8caf141ccdcd9745621
esG3 <esmeG3@users.noreply.github.com>
2024-01-17 20:52:19 +0000

    formatted weather and date using api

--- src/index.js
+++ src/index.js
@@ -3,11 +3,38 @@ let temperatureElement = document.querySelector("#temperature-today")
 let temperature = responce.data.temperature.current;
 let cityWeatherElement = document.querySelector("#city-weather");
 let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windSpeedElement = document.querySelector("#wind-speed");
+let timeElement = document.querySelector("#time");
+let date = new Date (responce.data.time * 1000);
 
-
+timeElement.innerHTML = formatDate(date);
 cityWeatherElement.innerHTML = responce.data.city;
 descriptionElement.innerHTML = responce.data.condition.description;
+humidityElement.innerHTML = `${responce.data.temperature.humidity}%`;
+windSpeedElement.innerHTML = `${responce.data.wind.speed} km/h`;
 temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+        "Sunday", 
+        "Monday",
+        "Tuesday", 
+        "Wednesday",
+        "Thursday", 
+        "Friday", 
+        "Saturday"
+    ];
+    let day = days[date.getDay()];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day}, ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
